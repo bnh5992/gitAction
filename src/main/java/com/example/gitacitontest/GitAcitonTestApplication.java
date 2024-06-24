79bed62bb5bf9b0c37f33133f7ba64027c052b0c
bnh5992 <skagus159632@naver.com>
2024-06-24 14:12:38 +0900

    test

---
+++ src/main/java/com/example/gitacitontest/GitAcitonTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitacitontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitAcitonTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitAcitonTestApplication.class, args);
+    }
+
+}
